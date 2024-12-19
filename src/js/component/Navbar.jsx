fa9b31ecee8889ef353cb1d6c224cbdf9dafc5e8
YPL4906 <104123424+YPL4906@users.noreply.github.com>
2024-12-19 15:53:54 +0000

    LandingPage

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function Navbar() {
 return (
-<nav className="navbar navbar-expand-lg navbar-light bg-dark">
+<nav className="navbar navbar-expand-lg navbar-dark bg-dark" data-bs-theme="dark">
   <div className="container">
     <a className="navbar-brand text-white" href="#">Start Bootstrap</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"><i className="bi bi-list"></i></span>
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
+    <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
       <ul className="navbar-nav ml-auto">
         <li className="nav-item active">
           <a className="nav-link active text-white" href="#">About<span className="sr-only"></span></a>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,33 +3,37 @@ import Navbar from "./Navbar";
 import Jumbotron from "./Jumbotron";
 import Card from "./Card";
 import Footer from "./Footer";
+import cardImage from "../../img/card-image.png"
 
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
-			<Navbar/>
-			<Jumbotron/>
+			<Navbar />
+			<Jumbotron />
 			<div>
-			<div className="container text-center">
-  <div className="row">
-    <div className="col">
-      <Card/>
-    </div>
-    <div className="col">
-	  <Card/> 
-    </div>
-    <div className="col">
-	  <Card/>
-    </div>
-  </div>
-</div>
+				<div className="container text-center">
+					<div className="row">
+						<div className="col-md-6 col-lg-3">
+							<Card cardImage={cardImage} />
+						</div>
+						<div className="col-md-6 col-lg-3">
+							<Card cardImage={cardImage}/>
+						</div>
+						<div className="col-md-6 col-lg-3">
+							<Card cardImage={cardImage}/>
+						</div>
+						<div className="col-md-6 col-lg-3">
+							<Card cardImage={cardImage}/>
+						</div>
+					</div>
+				</div>
+			</div>
+			<>
+				<Footer />
+			</>
 		</div>
-		<>
-		<Footer/>
-		</>
-</div>
 	);
 };
 
